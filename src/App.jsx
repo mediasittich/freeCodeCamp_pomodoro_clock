57b70e69af05cf9af294f55a3c8e6190cb00f6df
Regina Galambos <gina.galambos@gmail.com>
2020-01-21 23:41:28 +0100

    add Bootstrap via CDN for prototyping add basic layout remove content from App.css

--- src/App.jsx
+++ src/App.jsx
@@ -7,54 +7,68 @@ function App() {
   const [timerLeft, setTimerLeft] = useState(sessionLength);
 
   return (
-    <div className="App">
+    <div className="App container">
       <h1>Pomodoro Clock</h1>
 
-      <div>
-        <label id="break-label">Break Length</label>
-        <div>
-          <button
-            id="break-decrement"
-            onClick={() => setBreakLength(breakLength - 1)}
-          >
-            -
-          </button>
-          <div id="break-length">{breakLength}</div>
-          <button
-            id="break-increment"
-            onClick={() => setBreakLength(breakLength + 1)}
-          >
-            +
-          </button>
+      <div className="row">
+        <div className="col text-center">
+          <label id="break-label">Break Length</label>
+          <div className="d-flex justify-content-center align-items-center">
+            <button
+              className="btn btn-outline-dark"
+              id="break-decrement"
+              onClick={() => setBreakLength(breakLength - 1)}
+            >
+              -
+            </button>
+            <div id="break-length" className="px-2">
+              {breakLength}
+            </div>
+            <button
+              className="btn btn-outline-dark"
+              id="break-increment"
+              onClick={() => setBreakLength(breakLength + 1)}
+            >
+              +
+            </button>
+          </div>
         </div>
-      </div>
-      <div>
-        <label id="session-label">Session Length</label>
-        <div>
-          <button
-            id="session-decrement"
-            onClick={() => setSessionLength(sessionLength - 1)}
-          >
-            -
-          </button>
-          <div id="session-length">{sessionLength}</div>
-          <button
-            id="session-increment"
-            onClick={() => setSessionLength(sessionLength + 1)}
-          >
-            +
-          </button>
+        <div className="col text-center">
+          <label id="session-label">Session Length</label>
+          <div className="d-flex justify-content-center align-items-center">
+            <button
+              className="btn btn-outline-dark"
+              id="session-decrement"
+              onClick={() => setSessionLength(sessionLength - 1)}
+            >
+              -
+            </button>
+            <div id="session-length" className="px-2">
+              {sessionLength}
+            </div>
+            <button
+              className="btn btn-outline-dark"
+              id="session-increment"
+              onClick={() => setSessionLength(sessionLength + 1)}
+            >
+              +
+            </button>
+          </div>
         </div>
       </div>
 
-      <div>
+      <div className="d-flex flex-column align-items-center mt-5">
         <label id="timer-label">Session</label>
         <div id="time-left">{timerLeft}</div>
       </div>
 
-      <div>
-        <button id="start_stop">Start</button>
-        <button id="reset">Reset</button>
+      <div className="d-flex flex-row justify-content-center mt-3">
+        <button className="btn btn-dark mx-1" id="start_stop">
+          Start
+        </button>
+        <button className="btn btn-dark mx-1" id="reset">
+          Reset
+        </button>
       </div>
     </div>
   );
